b3e2746c2741f0bc17929ec38ce2eb1d591b07fa
juanc <juanc@192.168.0.20>
2019-10-17 21:58:03 -0500

    Primera version/ sin diseño/ sin registro

--- src/java/Control/Validacion.java
+++ src/java/Control/Validacion.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Controlador;
+package Control;
 
-import LoginMVC.modelo.DaoUser;
+import DAO.DaoUser;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
--- src/java/DAO/Dao.java
+++ src/java/DAO/Dao.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package LoginMVC.modelo;
+package DAO;
 
 import java.sql.DriverManager;
 import java.sql.DriverManager;
